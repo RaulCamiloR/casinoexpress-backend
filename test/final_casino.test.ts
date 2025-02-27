4ec00770961571b2d50fe1b9c0b6749fe92d025f
RaulCamiloR <camilo.rodriguez@cloudhesive.com>
2025-02-26 21:12:28 -0500

    Initial commit

---
+++ test/final_casino.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinalCasino from '../lib/final_casino-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/final_casino-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinalCasino.FinalCasinoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
